7aaebb3a043a0267133d361702b905659e36c1df
¨Anthony <anthony@laibe.cc>
2021-09-11 21:02:48 +0200

    Update dist

--- dist/main.js
+++ dist/main.js
@@ -6,8 +6,6 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -18,197 +16,189 @@ var _propTypes2 = _interopRequireDefault(_propTypes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+// NOTE --  Majority of this code is referenced from: https://github.com/alexvandesande/blockies
+//          Mostly to ensure congruence to Ethereum Mist's Identicons
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+// The random number is a js implementation of the Xorshift PRNG
+function seedrand(seed) {
+  var randseed = new Array(4); // Xorshift: [x, y, z, w] 32 bit values
 
-var Identicon = function (_Component) {
-  _inherits(Identicon, _Component);
-
-  function Identicon(props) {
-    _classCallCheck(this, Identicon);
-
-    var _this = _possibleConstructorReturn(this, (Identicon.__proto__ || Object.getPrototypeOf(Identicon)).call(this, props));
-
-    _this.generateIdenticon = _this.generateIdenticon.bind(_this);
-    return _this;
+  for (var i = 0; i < randseed.length; i++) {
+    randseed[i] = 0;
+  }
+  for (var _i = 0; _i < seed.length; _i++) {
+    randseed[_i % 4] = (randseed[_i % 4] << 5) - randseed[_i % 4] + seed.charCodeAt(_i);
   }
 
-  _createClass(Identicon, [{
-    key: 'componentDidMount',
-    value: function componentDidMount() {
-      this.generateIdenticon(_extends({}, this.props));
+  // based on Java's String.hashCode(), expanded to 4 32bit values
+  return function random() {
+    var t = randseed[0] ^ randseed[0] << 11;
+
+    randseed[0] = randseed[1];
+    randseed[1] = randseed[2];
+    randseed[2] = randseed[3];
+    randseed[3] = randseed[3] ^ randseed[3] >> 19 ^ t ^ t >> 8;
+
+    return (randseed[3] >>> 0) / (1 << 31 >>> 0);
+  };
+}
+
+function createColor(random) {
+  // saturation is the whole color spectrum
+  var h = Math.floor(random() * 360);
+  // saturation goes from 40 to 100, it avoids greyish colors
+  var s = random() * 60 + 40 + '%';
+  // lightness can be anything from 0 to 100, but probabilities are a bell curve around 50%
+  var l = (random() + random() + random() + random()) * 25 + '%';
+
+  var color = 'hsl(' + h + ',' + s + ',' + l + ')';
+  return color;
+}
+
+function createImageData(size, random) {
+  var width = size; // Only support square icons for now
+  var height = size;
+
+  var dataWidth = Math.ceil(width / 2);
+  var mirrorWidth = width - dataWidth;
+
+  var data = [];
+  for (var y = 0; y < height; y++) {
+    var row = [];
+    for (var x = 0; x < dataWidth; x++) {
+      // this makes foreground and background color to have a 43% (1/2.3) probability
+      // spot color has 13% chance
+      row[x] = Math.floor(random() * 2.3);
     }
-  }, {
-    key: 'componentWillUpdate',
-    value: function componentWillUpdate(nextProps) {
-      if (!this.isEquivalent(this.props, nextProps)) this.generateIdenticon(_extends({}, nextProps));
+    var r = row.slice(0, mirrorWidth);
+    r.reverse();
+    row = row.concat(r);
+
+    for (var i = 0; i < row.length; i++) {
+      data.push(row[i]);
     }
-  }, {
-    key: 'isEquivalent',
-    value: function isEquivalent(prevProps, nextProps) {
-      var aProps = Object.getOwnPropertyNames(prevProps);
-      var bProps = Object.getOwnPropertyNames(nextProps);
+  }
 
-      if (aProps.length != bProps.length) {
-        return false;
-      }
+  return data;
+}
 
-      for (var i = 0; i < aProps.length; i++) {
-        var propName = aProps[i];
+function drawCanvas(canvas, scale, _ref) {
+  var imageData = _ref.imageData,
+      color = _ref.color,
+      bgColor = _ref.bgColor,
+      spotColor = _ref.spotColor;
 
-        if (prevProps[propName] !== nextProps[propName]) {
-          return false;
-        }
-      }
+  var width = Math.sqrt(imageData.length);
+  var size = width * scale;
 
-      return true;
+  canvas.width = size;
+  canvas.style.width = size + 'px';
+
+  canvas.height = size;
+  canvas.style.height = size + 'px';
+
+  var cc = canvas.getContext('2d');
+  cc.fillStyle = bgColor;
+  cc.fillRect(0, 0, canvas.width, canvas.height);
+  cc.fillStyle = color;
+
+  for (var i = 0; i < imageData.length; i++) {
+    // if data is 2, choose spot color, if 1 choose foreground
+    cc.fillStyle = imageData[i] === 1 ? color : spotColor;
+
+    // if data is 0, leave the background
+    if (imageData[i]) {
+      var row = Math.floor(i / width);
+      var col = i % width;
+
+      cc.fillRect(col * scale, row * scale, scale, scale);
     }
-  }, {
-    key: 'generateIdenticon',
-    value: function generateIdenticon(options) {
-      // NOTE --  Majority of this code is referenced from: https://github.com/alexvandesande/blockies
-      //          Mostly to ensure congruence to Ethereum Mist's Identicons
+  }
+}
 
-      // The random number is a js implementation of the Xorshift PRNG
-      var randseed = new Array(4); // Xorshift: [x, y, z, w] 32 bit values
+function generateIdenticon(_ref2) {
+  var bgColor = _ref2.bgColor,
+      color = _ref2.color,
+      seed = _ref2.seed,
+      size = _ref2.size,
+      spotColor = _ref2.spotColor;
 
-      function seedrand(seed) {
-        for (var i = 0; i < randseed.length; i++) {
-          randseed[i] = 0;
-        }
-        for (var _i = 0; _i < seed.length; _i++) {
-          randseed[_i % 4] = (randseed[_i % 4] << 5) - randseed[_i % 4] + seed.charCodeAt(_i);
-        }
-      }
+  var random = seedrand(seed);
 
-      function rand() {
-        // based on Java's String.hashCode(), expanded to 4 32bit values
-        var t = randseed[0] ^ randseed[0] << 11;
+  // order matters since we are using random()
+  if (!color) color = createColor(random);
+  if (!bgColor) bgColor = createColor(random);
+  if (!spotColor) spotColor = createColor(random);
 
-        randseed[0] = randseed[1];
-        randseed[1] = randseed[2];
-        randseed[2] = randseed[3];
-        randseed[3] = randseed[3] ^ randseed[3] >> 19 ^ t ^ t >> 8;
+  return {
+    bgColor: bgColor,
+    color: color,
+    imageData: createImageData(size, random),
+    spotColor: spotColor
+  };
+}
 
-        return (randseed[3] >>> 0) / (1 << 31 >>> 0);
-      }
+var Identicon = _react2.default.memo(function Identicon(_ref3) {
+  var bgColor = _ref3.bgColor,
+      className = _ref3.className,
+      color = _ref3.color,
+      scale = _ref3.scale,
+      seed = _ref3.seed,
+      size = _ref3.size,
+      spotColor = _ref3.spotColor,
+      props = _objectWithoutProperties(_ref3, ['bgColor', 'className', 'color', 'scale', 'seed', 'size', 'spotColor']);
 
-      function createColor() {
-        // saturation is the whole color spectrum
-        var h = Math.floor(rand() * 360);
-        // saturation goes from 40 to 100, it avoids greyish colors
-        var s = rand() * 60 + 40 + '%';
-        // lightness can be anything from 0 to 100, but probabilities are a bell curve around 50%
-        var l = (rand() + rand() + rand() + rand()) * 25 + '%';
+  var canvasRef = (0, _react.useRef)();
 
-        var color = 'hsl(' + h + ',' + s + ',' + l + ')';
-        return color;
-      }
+  // Cache identiconData so we can use it to trigger a redraw.
+  var identiconData = (0, _react.useMemo)(function () {
+    return generateIdenticon({
+      bgColor: bgColor,
+      color: color,
+      seed: seed,
+      size: size,
+      spotColor: spotColor
+    });
+  }, [bgColor, color, seed, size, spotColor]);
 
-      function createImageData(size) {
-        var width = size; // Only support square icons for now
-        var height = size;
-
-        var dataWidth = Math.ceil(width / 2);
-        var mirrorWidth = width - dataWidth;
-
-        var data = [];
-        for (var y = 0; y < height; y++) {
-          var row = [];
-          for (var x = 0; x < dataWidth; x++) {
-            // this makes foreground and background color to have a 43% (1/2.3) probability
-            // spot color has 13% chance
-            row[x] = Math.floor(rand() * 2.3);
-          }
-          var r = row.slice(0, mirrorWidth);
-          r.reverse();
-          row = row.concat(r);
-
-          for (var i = 0; i < row.length; i++) {
-            data.push(row[i]);
-          }
-        }
-
-        return data;
-      }
-
-      function setCanvas(identicon, imageData, color, scale, bgcolor, spotcolor) {
-        var width = Math.sqrt(imageData.length);
-        var size = width * scale;
-
-        identicon.width = size;
-        identicon.style.width = size + 'px';
-
-        identicon.height = size;
-        identicon.style.height = size + 'px';
-
-        var cc = identicon.getContext('2d');
-        cc.fillStyle = bgcolor;
-        cc.fillRect(0, 0, identicon.width, identicon.height);
-        cc.fillStyle = color;
-
-        for (var i = 0; i < imageData.length; i++) {
-          // if data is 2, choose spot color, if 1 choose foreground
-          cc.fillStyle = imageData[i] === 1 ? color : spotcolor;
-
-          // if data is 0, leave the background
-          if (imageData[i]) {
-            var row = Math.floor(i / width);
-            var col = i % width;
-
-            cc.fillRect(col * scale, row * scale, scale, scale);
-          }
-        }
-      }
-
-      var opts = options || {};
-      var size = opts.size || 8;
-      var scale = opts.scale || 4;
-      var seed = opts.seed || Math.floor(Math.random() * Math.pow(10, 16)).toString(16);
-
-      seedrand(seed);
-
-      var color = opts.color || createColor();
-      var bgcolor = opts.bgColor || createColor();
-      var spotcolor = opts.spotColor || createColor();
-      var imageData = createImageData(size);
-      var canvas = setCanvas(this.identicon, imageData, color, scale, bgcolor, spotcolor);
-
-      return canvas;
+  // Redraw when scale or identiconData updates.
+  (0, _react.useEffect)(function () {
+    if (canvasRef.current) {
+      drawCanvas(canvasRef.current, scale, identiconData);
     }
-  }, {
-    key: 'render',
-    value: function render() {
-      var _this2 = this;
+  }, [identiconData, scale]);
 
-      return _react2.default.createElement('canvas', {
-        ref: function ref(identicon) {
-          _this2.identicon = identicon;
-        },
-        className: this.props.className
-      });
-    }
-  }]);
+  return _react2.default.createElement('canvas', _extends({
+    ref: function ref(canvas) {
+      canvasRef.current = canvas;
 
-  return Identicon;
-}(_react.Component);
-
-exports.default = Identicon;
-
+      // Redraw when the ref updates.
+      if (canvas) {
+        drawCanvas(canvas, scale, identiconData);
+      }
+    },
+    className: className
+  }, props));
+});
 
 Identicon.defaultProps = {
-  className: 'identicon'
+  className: 'identicon',
+  scale: 4,
+  seed: Math.floor(Math.random() * Math.pow(10, 16)).toString(16),
+  size: 8
 };
 
 Identicon.propTypes = {
-  seed: _propTypes2.default.string.isRequired,
-  size: _propTypes2.default.number,
-  scale: _propTypes2.default.number,
-  color: _propTypes2.default.string,
   bgColor: _propTypes2.default.string,
+  className: _propTypes2.default.string,
+  color: _propTypes2.default.string,
+  scale: _propTypes2.default.number,
+  seed: _propTypes2.default.string,
+  size: _propTypes2.default.number,
   spotColor: _propTypes2.default.string
 };
+
+exports.default = Identicon;
 
